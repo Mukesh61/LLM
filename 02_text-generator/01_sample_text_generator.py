176c74b1630b45f608755bb2121e50c659aaa884
Mukesh61 <45516247+Mukesh61@users.noreply.github.com>
2025-02-18 02:06:06 +0530

    Update 01_sample_text_generator.py

--- 02_text-generator/01_sample_text_generator.py
+++ 02_text-generator/01_sample_text_generator.py
@@ -5,3 +5,8 @@ text_gen_pipe = pipeline(task='text-generation', model='distilgpt2')
 input_promt = 'This is a story about Python programmer.'
 
 response = text_gen_pipe(input_promt, max_length = 200, pad_token_id=text_gen_pipe.tokenizer.eos_token_id)
+
+
+'''
+pad token id = end of sequence id, means end of sequence will end, to make a statement meaningful. 
+'''
