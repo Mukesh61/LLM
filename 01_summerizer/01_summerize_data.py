0786af08bb0c0fb41927558201e293b7f0c39b64
Mukesh61 <45516247+Mukesh61@users.noreply.github.com>
2025-02-18 01:40:22 +0530

    Update 01_summerize_data.py

--- 01_summerizer/01_summerize_data.py
+++ 01_summerizer/01_summerize_data.py
@@ -4,6 +4,6 @@ sumarizer_pipe = pipeline(task='summarization', model='facebook/bart-large-cnn')
 
 data = 'give story of Cinderella'
 
-summary = sumarizer_pipe(data, max_length = 60)
+summary = sumarizer_pipe(data, max_length = 60, clean_up_tokenization_spaces=True)
 
 print(summary)
