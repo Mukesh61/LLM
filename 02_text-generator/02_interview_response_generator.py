2852e35bd3e2796fed0016de16b945f0ee75bc5a
Mukesh61 <45516247+Mukesh61@users.noreply.github.com>
2025-02-18 02:15:17 +0530

    Create 02_interview_response_generator.py

---
+++ 02_text-generator/02_interview_response_generator.py
@@ -0,0 +1,17 @@
+from transformers import pipeline
+
+text_gen_pipe = pipeline(task='text-generation', model='distilgpt2')
+
+input_promt = 'Thanks for this opportunity. I got to learn something new about Pyspark today.'
+
+response = 'Thank you !'
+
+promt_msg = f'''Interview Experience of candidate: 
+{input_promt} 
+Interviewer response:
+{response}
+'''
+
+prompt_response = text_gen_pipe(promt_msg, max_length = 70, pad_token_id=text_gen_pipe.tokenizer.eos_token_id)
+
+print(prompt_response)
